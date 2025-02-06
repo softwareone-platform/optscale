04389b084ffa0fd148de8254d9476c2bea1b7973
Francesco Faraone <francesco.faraone@softwareone.com>
2025-02-06 12:08:01 +0100

    MPT-6748 fix jira issue creation fixversion and component are mandatory

--- .github/actions/create-jira-issue/main.py
+++ .github/actions/create-jira-issue/main.py
@@ -7,15 +7,21 @@ JIRA_USERNAME = os.getenv("INPUT_JIRA_USER_EMAIL")
 JIRA_API_TOKEN = os.getenv("INPUT_JIRA_API_TOKEN")
 PROJECT = os.getenv("INPUT_PROJECT")
 SUMMARY = os.getenv("INPUT_SUMMARY")
-DESCRIPTION = os.getenv("INPUT_DESCRIPTION", "")
+DESCRIPTION = os.getenv("INPUT_DESCRIPTION")
 ISSUE_TYPE = os.getenv("INPUT_ISSUE_TYPE")
+FIX_VERSION = os.getenv("INPUT_FIX_VERSION")
+COMPONENT = os.getenv("INPUT_COMPONENT")
 EPIC = os.getenv("INPUT_EPIC")
 ASSIGNEE = os.getenv("INPUT_ASSIGNEE")
 GITHUB_OUTPUT = os.getenv("GITHUB_OUTPUT")
 
 # Ensure required environment variables are set
-if not all([JIRA_URL, JIRA_USERNAME, JIRA_API_TOKEN, PROJECT, SUMMARY, ISSUE_TYPE]):
-    print("::error::Missing required input parameters: jira_base_url, jira_user_email, jira_api_token, project, summary, issue_type.")
+if not all([JIRA_URL, JIRA_USERNAME, JIRA_API_TOKEN, PROJECT, SUMMARY, ISSUE_TYPE, FIX_VERSION, COMPONENT]):
+    print(
+        "::error::Missing required input parameters: "
+        "jira_base_url, jira_user_email, jira_api_token, project, summary, issue_type, "
+        "fix_version, component."
+    )
     exit(1)
 
 # Jira API endpoint
@@ -34,10 +40,14 @@ payload = {
     "fields": {
         "project": {"key": PROJECT},
         "summary": SUMMARY,
-        "description": DESCRIPTION,
         "issuetype": {"name": ISSUE_TYPE},
+        "fixVersions": [{"name": FIX_VERSION}],
+        "components": [{"name": COMPONENT}]
     }
 }
+
+if DESCRIPTION:
+    payload["fields"]["description"] = {"value": DESCRIPTION}
 
 if ASSIGNEE:
     payload["fields"]["assignee"] = {"name": ASSIGNEE}
