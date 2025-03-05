0c0fa6b8af7de6eaa59071c2980134e14700f524
Steve Churchill <steve.churchill@softwareone.com>
2025-03-05 10:31:14 +0000

    Update interceptor.ts

--- e2etests/utils/interceptor.ts
+++ e2etests/utils/interceptor.ts
@@ -32,7 +32,7 @@ export async function interceptEventRequest(config: IInterceptorConfig): Promise
             });
             console.log(`Intercepted Event request for operationName ${requestPostData.operationName}`);
         } else {
-            await route.continue();
+            throw new Error(`Unexpected operationName: ${requestPostData.operationName}`);
         }
     });
 }
@@ -48,7 +48,7 @@ export async function interceptDataSourcesRequest(config: IInterceptorConfig): P
             });
             console.log(`Intercepted Event request for operationName ${requestPostData.operationName}`);
         } else {
-            await route.continue();
+            throw new Error(`Unexpected operationName: ${requestPostData.operationName}`);
         }
     });
 }
