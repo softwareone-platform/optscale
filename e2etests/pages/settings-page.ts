5ef4e50425346a25bb7005852508cddbd27925b8
Steve Churchill <steve.churchill@softwareone.com>
2025-03-07 09:50:52 +0000

    Update settings-page.ts

--- e2etests/pages/settings-page.ts
+++ e2etests/pages/settings-page.ts
@@ -1,16 +1,6 @@
 import {BasePage} from "./base-page";
 import {Locator, Page} from "@playwright/test";
 
-export class SettingsPage extends BasePage {
-    readonly heading: Locator;
-
-    constructor(page: Page) {
-        super(page, '/settings');
-        this.heading = this.page.locator('//h1[.="Settings"]');
-    }
-}import {BasePage} from "./base-page";
-import {Locator, Page} from "@playwright/test";
-
 /**
  * Represents the Settings Page.
  * Extends the BasePage class.
