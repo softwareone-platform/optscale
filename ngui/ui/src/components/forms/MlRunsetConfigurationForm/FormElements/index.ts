70e631613b69d70c8c9f87908fe64daf8c328791
ek-hystax <33006768+ek-hystax@users.noreply.github.com>
2025-05-06 10:02:15 +0400

    OSN-775. Add image and virtual environment path fields support to runset configuration form
    - Introduced an image field in the runset configuration form and input parameters component.
    - Introduced a virtual environment path field in the runset configuration form and input parameters component.
    - Updated constants, types, and utility functions to accommodate the new image and virtual environment path fields.
    - Enhanced the UI to display the image value and the virtual environment path in the input parameters overview.

--- ngui/ui/src/components/forms/MlRunsetConfigurationForm/FormElements/index.ts
+++ ngui/ui/src/components/forms/MlRunsetConfigurationForm/FormElements/index.ts
@@ -3,11 +3,13 @@ import CommandToExecuteField from "./CommandToExecuteField";
 import DataSourceField from "./DataSourceField";
 import FormButtons from "./FormButtons";
 import HyperparametersField from "./HyperparametersField";
+import ImageField from "./ImageField";
 import InstanceTypeField from "./InstanceTypeField";
 import MaximumParallelRunsField from "./MaximumParallelRunsField";
 import RegionField from "./RegionField";
 import SpotInstanceFields from "./SpotInstanceFields";
 import TaskField from "./TaskField";
+import VirtualEnvironmentPathField from "./VirtualEnvironmentPathField";
 
 export {
   TaskField,
@@ -19,5 +21,7 @@ export {
   CommandToExecuteField,
   AbortConditionsFields,
   FormButtons,
-  SpotInstanceFields
+  SpotInstanceFields,
+  ImageField,
+  VirtualEnvironmentPathField
 };
