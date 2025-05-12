b0f2beda9935db386ff94cfa985e79a8c82d5ec3
dawid.wolosz <dawid.wolosz@softwareone.com>
2025-05-12 12:08:14 +0200

    Removed unused `.env.example`, updated `.gitignore`, and adjusted test descriptions for clarity. Consolidated regression test configs, streamlined snapshot paths, and improved docker-compose volume mapping. Enhanced maintainability and ensured clear separation of concerns.
    Replaced `OrganizationCleanExpansesResponse` with `OptimizationsResponse` to intercept api optimizations response data.
    
    Update Playwright to version 1.52.0 in Dockerfile, updated reference snapshots
    
    Add element dimension rounding utility and update tests
    
    Update Playwright tests from screenshots to regression focus
    
    Refactor scripts, test descriptors, and configurations to shift focus from screenshot to regression naming convention.
    
    Added a delay which is controlled by the SCREENSHOT_UPDATE_DELAY variable. Scripts have been updated use cross-env and re-enabled previously commented out browser config. Includes screenshots for these browsers on Windows.
    
    * Corrected test.
    
    * Update base-page.ts
    
    * Fixed typo
    
    Refactor E2E test structure and update Docker integration
    
    updated package.json
    
    Minor playwright.screenshot.config.ts fix

--- e2etests/playwright.config.ts
+++ e2etests/playwright.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   globalSetup: "./setup/global-setup.ts",
   globalTeardown: "./setup/global-teardown.ts",
   testDir: '../e2etests',
+  testIgnore: ['**/screenshots/**'],
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -27,12 +28,6 @@ export default defineConfig({
     ["json", {outputFile: "results.json"}],
     ["html", {open: "never"}],
   ],
-  expect: {
-    toHaveScreenshot: {
-      animations: "disabled",
-      stylePath: './fixtures/pre-screenshot-styles.css'
-    },
-  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     actionTimeout: 10000,
@@ -56,6 +51,20 @@ export default defineConfig({
       name: "chrome",
       use: {
         channel: "chrome",
+      },
+      dependencies: ["setup"],
+    },
+    {
+      name: "firefox",
+      use: {
+        browserName: "firefox",
+      },
+      dependencies: ["setup"],
+    },
+    {
+      name: "Microsoft Edge",
+      use: {
+        channel: "msedge",
       },
       dependencies: ["setup"],
     },
