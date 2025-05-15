40cfbfc2305342023d42937790b8fa7572a6e9de
dawid.wolosz <dawid.wolosz@softwareone.com>
2025-05-15 22:10:35 +0200

    added docker image create in package.json

--- e2etests/playwright.config.ts
+++ e2etests/playwright.config.ts
@@ -4,31 +4,21 @@ import dotenv from 'dotenv';
 import path from 'path';
 dotenv.config({path: path.resolve(__dirname, '.env.local')});
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   globalSetup: "./setup/global-setup.ts",
   globalTeardown: "./setup/global-teardown.ts",
   testDir: '../e2etests',
   testIgnore: ['**/screenshots/**'],
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 3,
-  /* Individual test timeout,test.slow() annotation triples this value for decorated tests*/
   timeout: 30000,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["list"],
     ["json", {outputFile: "results.json"}],
     ["html", {open: "never"}],
   ],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     actionTimeout: 10000,
     baseURL: process.env.BASE_URL,
