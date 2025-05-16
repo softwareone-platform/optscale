bb8a88b419e617e40b8dbb6d77a8cf4fe60411ae
dawid.wolosz <dawid.wolosz@softwareone.com>
2025-05-16 17:45:24 +0200

    Added localhost detection in during docker run

--- e2etests/playwright.screenshot.config.ts
+++ e2etests/playwright.screenshot.config.ts
@@ -1,18 +1,33 @@
 import {defineConfig} from '@playwright/test';
 import os from 'os';
-const platform = os.platform();
-
 import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config({path: path.resolve(__dirname, '.env.local')});
+
+// Load .env.local
+dotenv.config({ path: path.resolve(__dirname, '.env.local') });
+
+// Detect if running in Docker
+const isDocker = process.env.DOCKER_ENV === 'true';
+
+// Determine baseURL
+const getBaseURL = (): string => {
+  const originalURL = process.env.BASE_URL || 'http://localhost:3000';
+
+  if (isDocker && originalURL.startsWith('http://localhost')) {
+    return originalURL.replace('localhost', 'host.docker.internal');
+  }
+
+  return originalURL;
+};
+
 
 export default defineConfig({
   testDir: './/',
   testMatch: /swo-screenshot-tests\.spec\.ts/,
-  snapshotPathTemplate: `./tests/screenshots/snapshots/${platform}/{arg}{ext}`,
+  snapshotPathTemplate: `./tests/screenshots/snapshots/${os.platform()}/{arg}{ext}`,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 1,
+  retries: 1,
   workers: process.env.CI ? 1 : 3,
   timeout: 30000,
   reporter: [
@@ -28,8 +43,8 @@ export default defineConfig({
   },
 
   use: {
+    baseURL: getBaseURL(),
     actionTimeout: 10000,
-    baseURL: process.env.BASE_URL,
     testIdAttribute: 'data-test-id',
     headless: true,
     trace: "retain-on-failure",
