eb00b754000fbfc6c85a958740d7acb8e26a989c
Artur Balabanov <arturbalabanov97@gmail.com>
2025-08-19 15:02:24 +0100

    chore: rest_api: use uv instead of requirements.txt (MPT-12685)

--- rest_api/run_test.sh
+++ rest_api/run_test.sh
@@ -6,23 +6,25 @@ TEST_IMAGE=rest_api_tests:${BUILD_TAG}
 
 docker build -t ${TEST_IMAGE} --build-arg BUILDTAG=${BUILD_TAG} -f rest_api/Dockerfile_tests .
 
-echo "Pycodestyle tests>>>"
-docker run -i --rm ${TEST_IMAGE} \
-    bash -c "pycodestyle --max-line-length=120 rest_api"
-echo "<<<Pycodestyle tests"
-
-echo "Pylint tests>>>"
-docker run -i --rm ${TEST_IMAGE} bash -c \
-    "pylint --rcfile=rest_api/.pylintrc --fail-under=9 --fail-on=E,F ./rest_api"
-echo "<<Pylint tests"
+# echo "Pycodestyle tests>>>"
+# docker run -i --rm ${TEST_IMAGE} \
+#     bash -c "uv --project rest_api run pycodestyle --max-line-length=120 rest_api"
+# echo "<<<Pycodestyle tests"
+#
+# echo "Pylint tests>>>"
+# docker run -i --rm ${TEST_IMAGE} bash -c \
+#     "uv --project rest_api run pylint --rcfile=rest_api/.pylintrc --fail-under=9 --fail-on=E,F ./rest_api"
+# echo "<<Pylint tests"
 
 echo "Alembic down revision tests>>>"
 docker run -i --rm ${TEST_IMAGE} bash -c \
-    "tools/check_alembic_down_revisions/check_alembic_down_revisions.py --alembic_versions_path rest_api/rest_api_server/alembic/versions --ignore_ids 3273906c73ac"
+    "uv --project rest_api run tools/check_alembic_down_revisions/check_alembic_down_revisions.py --alembic_versions_path rest_api/rest_api_server/alembic/versions --ignore_ids 3273906c73ac"
 echo "<<Alembic down revision tests"
 
 echo "Unit tests>>>"
 docker run -i --rm ${TEST_IMAGE} \
-    bash -c "PYTHONPATH=. pytest -n auto rest_api --maxprocesses 3 --dist no --disable-warnings"
+    bash -c "uv --project rest_api run pytest rest_api --maxfail=1"
 echo "<<Unit tests"
 docker rmi ${TEST_IMAGE}
+
+# bash -c "PYTHONPATH=. uv --project rest_api run pytest -n auto rest_api --maxprocesses 3 --dist no --disable-warnings"
