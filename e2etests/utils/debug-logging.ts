08fb3b6b4c1e9a47f4033aa0aa3ae6a48edfc877
Steve Churchill <steve.churchill@softwareone.com>
2025-09-05 14:07:00 +0100

    Update debug-logging.ts

--- e2etests/utils/debug-logging.ts
+++ e2etests/utils/debug-logging.ts
@@ -1,5 +1,3 @@
-import { DataSourceResponse } from "../test-data/test-data-response-types";
-
 export function debugLog(message: string) {
     if (process.env.DEBUG_LOG === 'true') {
         console.debug(`[DEBUG] ${message}`);
