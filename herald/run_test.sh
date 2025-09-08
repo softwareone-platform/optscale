43c637c39e63037cbd1b3a397f5905d98a03b696
Artur Balabanov <arturbalabanov97@gmail.com>
2025-09-08 14:48:14 +0100

    feat: ci: fix herald (MPT-12685)

--- herald/run_test.sh
+++ herald/run_test.sh
@@ -7,8 +7,8 @@ TEST_IMAGE=herald_tests:${BUILD_TAG}
 docker build -t ${TEST_IMAGE} --build-arg BUILDTAG=${BUILD_TAG} -f herald/Dockerfile_tests .
 
 echo "Linting>>>"
-docker run -i --rm ${TEST_IMAGE} \
-    bash -c "ruff format --config pyproject.toml --check --diff herald/ && ruff check --config pyproject.toml herald/"
+docker run -i --rm ${TEST_IMAGE} bash -c "uv --project herald run ruff format --config herald/pyproject.toml --check --diff herald/"
+docker run -i --rm ${TEST_IMAGE} bash -c "uv --project herald run ruff check --config herald/pyproject.toml herald/"
 echo "<<<Linting"
 
 echo "Alembic down revision tests>>>"
@@ -17,7 +17,7 @@ docker run -i --rm ${TEST_IMAGE} bash -c \
 echo "<<Alembic down revision tests"
 
 echo "Unit tests>>>"
-docker run -i --rm ${TEST_IMAGE} pytest
+docker run -i --rm ${TEST_IMAGE} bash -c "uv --project herald run pytest"
 echo "<<Unit tests"
 
 docker rmi ${TEST_IMAGE}
