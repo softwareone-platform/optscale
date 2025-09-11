f110db1cfb51b006f931b7ec4b5867e26a6e99bf
dawid.wolosz <dawid.wolosz@softwareone.com>
2025-09-11 10:09:21 +0200

    feat: update translation imports to improve structure and override handling

--- ffc_ngui/ui/src/translations/en-US/index.ts
+++ ffc_ngui/ui/src/translations/en-US/index.ts
@@ -2,35 +2,25 @@
  * Importing translation JSON files for the application.
  *
  * The imports are divided into two categories:
- * 1. `Main` translations: These are the base translations located in a shared directory.
+ * 1. `mainThemeTranslations` translations: These are the base translations located in a shared directory.
  * 2. Local translations: These are the translations specific to the current module.
  *
  * The `Main` translations are imported using relative paths that point to the shared directory.
- * The local translations are imported from the current directory.
+ * The local translations are imported from the current directory and override mainThemeTranslations properties.
  */
 
-// Importing main translation files from the shared directory
-import appMain from "../../../../../ngui/ui/src/translations/en-US/app.json";
-import currenciesMain from "../../../../../ngui/ui/src/translations/en-US/currencies.json";
-import errorMain from "../../../../../ngui/ui/src/translations/en-US/errors.json";
-import finOpsMain from "../../../../../ngui/ui/src/translations/en-US/finops.json";
-import successMain from "../../../../../ngui/ui/src/translations/en-US/success.json";
-// Importing local translation files from the current directory
-import app from "./app.json";
-import currencies from "./currencies.json";
-import error from "./errors.json";
-import finOps from "./finops.json";
-import success from "./success.json";
+import mainThemeTranslations from "../../../../../ngui/ui/src/translations/en-US";
+import appOverride from "./app.json";
+import currenciesOverride from "./currencies.json";
+import errorOverride from "./errors.json";
+import finOpsOverride from "./finops.json";
+import successOverride from "./success.json";
 
-// Each pair of main and local translations is combined into a single object.
-// This ensures that local translations override the main translations where keys overlap.
-
-const appMerged = { ...appMain, ...app }; // Merges main and local app translations
-const errorMerged = { ...errorMain, ...error }; // Merges main and local error translations
-const successMerged = { ...successMain, ...success }; // Merges main and local success translations
-const finOpsMerged = { ...finOpsMain, ...finOps }; // Merges main and local finOps translations
-const currenciesMerged = { ...currenciesMain, ...currencies }; // Merges main and local currencies translations
-
-const allTranslations = { ...appMerged, ...errorMerged, ...successMerged, ...finOpsMerged, ...currenciesMerged };
-
-export default allTranslations;
+export default {
+  ...mainThemeTranslations,
+  ...appOverride,
+  ...currenciesOverride,
+  ...errorOverride,
+  ...finOpsOverride,
+  ...successOverride
+};
