bfb4f5f43bdc98ddaeb9804a66e4461cc30d3fc0
dawid.wolosz <dawid.wolosz@softwareone.com>
2026-01-09 12:17:48 +0100

    Added theme translations override, added side menu to theme, added main layouts, added assest images support for typescript

---
+++ ngui/ui/src/themes/finops/components/Logo/Logo.tsx
@@ -0,0 +1,31 @@
+import Link from "@mui/material/Link";
+import { Link as RouterLink } from "react-router-dom";
+import logoDemo from "@theme/assets/logo/swo-logo-demo.svg";
+import logo from "@theme/assets/logo/swo-logo.svg";
+import { HOME } from "urls";
+
+interface LogoProps {
+  dataTestId: string;
+  demo?: boolean;
+  active?: boolean;
+  width?: string | number;
+  height?: string | number;
+}
+
+const getLogo = (demo: boolean) => (demo ? logoDemo : logo);
+const Logo = ({ dataTestId, demo = false, active = false, width = "auto", height = "auto" }: LogoProps) => {
+  /* MPT_TODO: Add 'swo-logo' translation */
+  // const intl = useIntl();
+
+  const renderLogo = <img width={width} height={height} src={getLogo(demo)} alt={"swo-logo"} data-test-id={dataTestId} />;
+
+  return active ? (
+    <Link component={RouterLink} sx={{ display: "flex" }} to={HOME}>
+      {renderLogo}
+    </Link>
+  ) : (
+    renderLogo
+  );
+};
+
+export default Logo;
---
+++ ngui/ui/src/themes/finops/layouts/BaseLayout/BaseLayout.styles.ts
@@ -0,0 +1,107 @@
+import { makeStyles } from "tss-react/mui";
+import { MPT_GRADIENT, SPACING_1 } from "@theme/utils/layouts";
+
+export const DRAWER_WIDTH = 250;
+
+const useStyles = makeStyles()((theme) => ({
+  appBar: {
+    boxShadow: "none"
+  },
+  headerSpacer: {
+    background: MPT_GRADIENT,
+    height: "3px"
+  },
+  toolbar: {
+    display: "flex",
+    height: "75px",
+    alightItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "white",
+    paddingLeft: "21px",
+    paddingRight: "21px"
+  },
+  logo: {
+    [theme.breakpoints.down("md")]: {
+      flex: 1
+    },
+    display: "flex",
+    gap: "30px",
+    alignItems: "center"
+  },
+  headerTitle: {
+    fontSize: "26px",
+    color: "black",
+    fontWeight: "bold",
+    alignItems: "center",
+    gap: "10px",
+    justifyContent: "middle",
+    display: "flex"
+  },
+  marginRight1: {
+    marginRight: theme.spacing(SPACING_1)
+  },
+  marginLeft1: {
+    marginLeft: theme.spacing(SPACING_1)
+  },
+  drawerPaper: {
+    position: "relative",
+    width: DRAWER_WIDTH,
+    paddingTop: theme.spacing(SPACING_1)
+  },
+  content: {
+    overflowY: "auto",
+    paddingLeft: "0",
+    paddingRight: "0",
+    maxWidth: "none",
+    // that way PageContentWrapper will fill whole page height
+    display: "flex",
+    flexDirection: "column"
+  },
+  mobileRegisterButton: {
+    backgroundColor: theme.palette.success.main,
+    color: "inherit"
+  },
+  layoutWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh"
+  },
+  menuAndContentWrapper: {
+    overflow: "hidden",
+    flexGrow: 1,
+    display: "flex"
+  },
+  wrapper: {
+    overflow: "hidden",
+    display: "grid",
+    gridTemplateColumns: "minmax(0, 1fr) 0",
+    [theme.breakpoints.up("md")]: {
+      transition: "grid-template-columns 0.2s ease-in"
+    }
+  },
+  wrapperWithDocsOpened: {
+    [theme.breakpoints.up("md")]: {
+      gridTemplateColumns: `minmax(0, 1fr) 400px`
+    },
+    gridTemplateColumns: `0 minmax(0, 1fr)`
+  },
+  hideableLayoutWrapper: {
+    [theme.breakpoints.down("md")]: {
+      visibility: "hidden"
+    }
+  },
+  preloaderOverlay: {
+    position: "absolute",
+    zIndex: 100000,
+    width: "100%",
+    height: "100%",
+    background: "#ffffff",
+    top: 0,
+    left: 0,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+}));
+
+export default useStyles;
