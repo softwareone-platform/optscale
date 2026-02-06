3481cd51bc29c202feb0c1f646054f4b3c7341bb
Dawo <dwolosz@dawo.pl>
2026-02-06 15:07:51 +0100

    Update ngui/ui/src/themes/finops/components/SearchInput/SearchInput.tsx
    Co-authored-by: michal-zielonka <146939528+michal-zielonka@users.noreply.github.com>

--- ngui/ui/src/themes/finops/components/SearchInput/SearchInput.tsx
+++ ngui/ui/src/themes/finops/components/SearchInput/SearchInput.tsx
@@ -1,16 +1,16 @@
 import { ComponentProps } from "react";
 import { Box } from "@mui/material";
-import SearchInputOriginal from "@main/components/SearchInput/SearchInput";
+import SearchInputBase from "@main/components/SearchInput/SearchInput";
 import useStyles from "./SearchInputOverride.styles";
 
-type SearchInputProps = ComponentProps<typeof SearchInputOriginal>;
+type SearchInputProps = ComponentProps<typeof SearchInputBase>;
 
 const SearchInput = (props: SearchInputProps) => {
   const { classes } = useStyles();
 
   return (
     <Box className={classes.searchInputWrapper}>
-      <SearchInputOriginal {...props} />
+      <SearchInputBase {...props} />
     </Box>
   );
 };
