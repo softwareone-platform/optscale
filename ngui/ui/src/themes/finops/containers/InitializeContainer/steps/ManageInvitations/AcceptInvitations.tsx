9eea17e36fc43820a080beccd9d42c28afe9fa19
Dawo <dwolosz@dawo.pl>
2026-02-16 10:59:38 +0100

    Feature/mpt 14721 fix initial invitation flow (#359)
    * feat: add AcceptInvitations and StepContainer components for handling invitations in theme
    
    * feat: add AcceptInvitations and StepContainer components for handling invitations in theme
    
    * feat: removed pdf override in theme
    
    * feat: add useOptscaleRecommendations hook for managing various recommendations

---
+++ ngui/ui/src/themes/finops/containers/InitializeContainer/steps/ManageInvitations/AcceptInvitations.tsx
@@ -0,0 +1,91 @@
+import { NetworkStatus } from "@apollo/client";
+import NavigationIcon from "@mui/icons-material/Navigation";
+import { Box } from "@mui/material";
+import { makeStyles } from "tss-react/mui";
+import ButtonLoader from "components/ButtonLoader";
+import Invitations from "components/Invitations";
+import { Error, Loading } from "containers/InitializeContainer/common";
+import { useOrganizationsQuery } from "graphql/__generated__/hooks/restapi";
+import { useIsFeatureEnabled } from "hooks/useIsFeatureEnabled";
+import { SPACING_1, SPACING_2 } from "utils/layouts";
+
+const useStyles = makeStyles()((theme) => ({
+  dashboardButton: {
+    [theme.breakpoints.up("md")]: {
+      position: "absolute",
+      right: 40,
+      bottom: 40
+    },
+    padding: theme.spacing(SPACING_2),
+    "& > *:not(:last-child)": {
+      marginRight: theme.spacing(SPACING_1)
+    }
+  }
+}));
+
+const AcceptInvitations = ({ invitations, refetchInvitations, onProceed }) => {
+  const { classes } = useStyles();
+  const isOrganizationCreationAllowed = useIsFeatureEnabled("organization_creation_allowed");
+
+  const {
+    data,
+    networkStatus: getOrganizationsNetworkStatus,
+    error: getOrganizationsError,
+    refetch: refetchOrganizations
+  } = useOrganizationsQuery({
+    fetchPolicy: "network-only",
+    notifyOnNetworkStatusChange: true
+  });
+
+  const getOrganizationsLoading = getOrganizationsNetworkStatus === NetworkStatus.loading;
+  const getOrganizationsRefetching = getOrganizationsNetworkStatus === NetworkStatus.refetch;
+
+  if (getOrganizationsLoading || getOrganizationsRefetching) {
+    return <Loading />;
+  }
+
+  if (getOrganizationsError) {
+    return <Error />;
+  }
+
+  const userHasOrganizations = data && data.organizations && data.organizations.length > 0;
+  const redirectToPendingInvitations = !isOrganizationCreationAllowed && !userHasOrganizations;
+
+  if (redirectToPendingInvitations) {
+    return onProceed();
+  }
+
+  return (
+    <>
+      <Box width={{ sm: "600px", md: "900px", lg: "1200px" }}>
+        <Invitations
+          widget
+          invitations={invitations}
+          styleProps={{ buttonsJustifyContent: "center" }}
+          onSuccessAccept={() => {
+            refetchOrganizations();
+            refetchInvitations();
+          }}
+          onSuccessDecline={() => {
+            refetchInvitations();
+          }}
+          isLoading={false}
+        />
+      </Box>
+      <Box>
+        <ButtonLoader
+          dataTestId="btn_proceed_to_optscale"
+          messageId="proceedToOptScale"
+          size="medium"
+          color="primary"
+          variant="contained"
+          onClick={onProceed}
+          startIcon={<NavigationIcon />}
+          customWrapperClass={classes.dashboardButton}
+        />
+      </Box>
+    </>
+  );
+};
+
+export default AcceptInvitations;
