57646a4f9609bf237c6c992abe08df8ce0baa6bb
dawid.wolosz <dawid.wolosz@softwareone.com>
2026-09-01 14:57:53 +0200

    feat(DateRangePicker): add Day and Month components with styling supporting max/min date and prev/next month days grayout

---
+++ ngui/ui/src/themes/finops/components/DateRangePicker/Day/Day.tsx
@@ -0,0 +1,36 @@
+import { IconButton, Typography } from "@mui/material";
+import useStyles from "./Day.styles";
+
+const Day = ({ disabled, highlighted, outlined, filled, outsideMonth, outsideBounds, onClick, onHover, value }) => {
+  const { classes, cx } = useStyles();
+  return (
+    <div
+      className={cx(classes.buttonContainer, {
+        [classes.highlighted]: !disabled && highlighted
+      })}
+    >
+      <IconButton
+        className={cx(classes.button, {
+          [classes.outlined]: !disabled && outlined,
+          [classes.filled]: !disabled && filled
+        })}
+        disabled={disabled}
+        onClick={onClick}
+        onMouseOver={onHover}
+      >
+        <Typography
+          className={cx(classes.buttonText, {
+            [classes.contrast]: !disabled && filled,
+            [classes.outsideMonthText]: outsideMonth,
+            [classes.outsideBoundsText]: outsideBounds
+          })}
+          disabled={disabled}
+        >
+          {value}
+        </Typography>
+      </IconButton>
+    </div>
+  );
+};
+
+export default Day;
