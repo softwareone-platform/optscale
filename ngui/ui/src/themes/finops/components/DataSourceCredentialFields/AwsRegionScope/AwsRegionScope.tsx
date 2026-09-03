6aa542eaf4bb6b029dd3dcff957cf48d97d273ec
dawid.wolosz <dawid.wolosz@softwareone.com>
2026-09-03 12:33:16 +0200

    fix: adjust padding in AwsRegionScope component for improved layout

--- ngui/ui/src/themes/finops/components/DataSourceCredentialFields/AwsRegionScope/AwsRegionScope.tsx
+++ ngui/ui/src/themes/finops/components/DataSourceCredentialFields/AwsRegionScope/AwsRegionScope.tsx
@@ -2,7 +2,7 @@ import Box from "@mui/material/Box";
 import BaseAwsRegionScope from "@main/components/DataSourceCredentialFields/AwsRegionScope/AwsRegionScope";
 
 const AwsRegionScope = () => (
-  <Box sx={{ borderTop: 1, borderBottom: 1, marginTop: 3, marginBottom: 2, borderColor: "divider" }}>
+  <Box sx={{ borderTop: 1, borderBottom: 1, marginTop: 3, marginBottom: 2, paddingBottom: 1, borderColor: "divider" }}>
     <BaseAwsRegionScope />
   </Box>
 );
